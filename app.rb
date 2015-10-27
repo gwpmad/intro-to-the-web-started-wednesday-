34d9ff920a29f49406741da4f0211b24b59fb490
Deon Tan <deonjtan@gmail.com>
2015-10-27 19:09:47 +0000

    Post method used

--- app.rb
+++ app.rb
@@ -16,12 +16,23 @@ get '/penguin' do
   erb :penguin
 end
 
+post '/named-cat' do
+  @name = p params[:name]
+  erb :index
+end
+
 get '/named-cat' do
   @name = p params[:name]
   erb :index
 end
 
+
 get '/random-cat' do
   @name = %w(Amigo Oscar Viking).sample
   erb :index
+end
+
+get '/cat-form' do
+  # @name = p params[:name]
+  erb :cat_form
 end
