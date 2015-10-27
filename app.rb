18aafed7c82245a0718dd363fe1b50593452c16b
Deon Tan <deonjtan@gmail.com>
2015-10-27 12:10:58 +0000

    html / css embedded into app.rb

--- app.rb
+++ app.rb
@@ -11,3 +11,17 @@ end
 get '/potato' do
   "potato"
 end
+
+get '/penguin' do
+  erb :penguin
+end
+
+get '/cat' do
+  # erb :index
+"
+  <div style='border: 20px dashed gray; width: 454px'>
+  <img src = http://bit.ly/1eze8aE>
+  </div>
+  Waddup!
+"
+end
